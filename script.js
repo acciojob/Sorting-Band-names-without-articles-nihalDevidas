2aba84818c02b4e2dfaed620a0564a63bb3cc02d
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-07 23:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
+function Sortingband(arr){
+	let sorted_array = arr.sort((x,y)=>{
+		if(x > y){
+			return 1;
+		}
+		return -1;
+	});
 
+  return sorted_array;
+}
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+let output = Sortingband(touristSpots);
+
+output.forEach((value)=>{
+
+	const elemento = document.creatElement("li");
+	document.getElementById("band").appendChild(elemento);
+})
